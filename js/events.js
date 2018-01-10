8a30075bbcb1fff03f36d74b3cb4d52867dbf8f1
Stephen Thurman <thurman024@gmail.com>
2018-01-10 02:19:44 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,29 @@
 //define functions here
+function getIt() {
+  $('p').click()
+  alert("Hey!")
+  return
+}
+function frameIt() {
+  $('img').on('load', function(){
+    <class = "tasty">
+  })
+  return
+}
+function pressIt() {
+  $('form').on('keydown', function(key) {
+    if(key.which == 71) {
+      alert("G was pressed");
+    }
+  });
+  return
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+getIt()
+frameIt()
+pressIt()
+submitIt()
 });
