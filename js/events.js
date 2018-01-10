22d0fa98213d34e3bf4f2bc6d0e9a2a3eef3b816
Stephen Thurman <thurman024@gmail.com>
2018-01-10 02:37:47 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
+function getIt() {
+  $('p').click()
+  alert("Hey!")
+  return
+}
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+  })
+  return
+}
+function pressIt() {
+  $('form').on('keydown', function(key) {
+    if(key.which == 71) {
+      alert("G was pressed");
+    }
+  });
+  return
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert("Your form is going to be submitted now.")
+  })
+  return
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+getIt()
+frameIt()
+pressIt()
+submitIt()
 });
